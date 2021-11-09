d4b89adb8256990c3470713bc3c41ed0926da2ab
Dan P <73758508+benochi@users.noreply.github.com>
2021-11-09 16:26:17 -0600

    Update app.js

--- promises/promise1/app.js
+++ promises/promise1/app.js
@@ -1,21 +1,21 @@
-let favNumber = 5;
-let baseURL = "http://numbersapi.com";
+const FAV_NUMBER = 5;
+const BASE_URL = "http://numbersapi.com";
 
 // 1.
-$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+$.getJSON(`${BASE_URL}/${FAV_NUMBER}?json`).then(data => {
   console.log(data);
 });
 
 // 2.
 let favNumbers = [7, 11, 22];
-$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+$.getJSON(`${BASE_URL}/${favNumbers}?json`).then(data => {
   console.log(data);
 });
 
 // 3.
 Promise.all(
   Array.from({ length: 4 }, () => {
-    return $.getJSON(`${baseURL}/${favNumber}?json`);
+    return $.getJSON(`${BASE_URL}/${FAV_NUMBER}?json`);
   })
 ).then(facts => {
   facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
